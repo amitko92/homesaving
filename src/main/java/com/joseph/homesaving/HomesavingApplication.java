95d6def339353e53b56ac0684e6538b8190a55c7
Amit Koren <30484152+amitko92@users.noreply.github.com>
2024-11-01 23:08:19 +0200

    init commit

---
+++ src/main/java/com/joseph/homesaving/HomesavingApplication.java
@@ -0,0 +1,13 @@
+package com.joseph.homesaving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomesavingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomesavingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseph/homesaving/HomesavingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseph.homesaving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomesavingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
